74d6a7df6b384b4e5bfebdaa7473697350bb8646
Artikey <remycharles22@gmail.com>
2018-09-29 13:35:10 +0200

    Cleaned the code

--- Board.py
+++ Board.py
@@ -4,14 +4,14 @@ from Btn import Btn
 class Board:
     def __init__(self):
         self.dictTrou = {1:[4,blanc],
-                        2:[4,blanc],
-                        3:[4,blanc],
-                        4:[4,blanc],
-                        5:[4,blanc],
-                        6:[4,blanc],
-                        7:[1,blanc],
-                        8:[4,blanc],
-                        9:[4,blanc],
+                         2:[4,blanc],
+                         3:[4,blanc],
+                         4:[4,blanc],
+                         5:[4,blanc],
+                         6:[4,blanc],
+                         7:[1,blanc],
+                         8:[4,blanc],
+                         9:[4,blanc],
                         10:[4,blanc],
                         11:[4,blanc],
                         12:[4,blanc]
@@ -22,14 +22,14 @@ class Board:
             self.btnList.append(Btn(-35+110*a,400,100,50))
             a += 1
         self.scorePlayer = 0
-        self.scoreFDP = 0
+        self.scoreEnemy = 0
 
     def draw(self):
         window.fill((0,0,0))
         window.blit(fontSize(45).render("Player: "+str(self.scorePlayer), True, blanc),(100,100))
-        window.blit(fontSize(45).render("FDP: "+str(self.scoreFDP), True, blanc),(700,100))
+        window.blit(fontSize(45).render("Enemy: "+str(self.scoreEnemy), True, blanc),(700,100))
         pygame.draw.rect(window,blanc,(50,392,700,5))
-        
+
         trot=0
         for t in self.dictTrou:
             trot+=1
@@ -43,7 +43,7 @@ class Board:
                 trot+=1
                 pygame.draw.ellipse(window, self.dictTrou[t][1],(735-(110*trot),340,100,50))
                 window.blit(fontSize(45).render(str(self.dictTrou[t][0]), True, noir),(760-(110*trot),350))
-        
+
 
     """ not GUI part"""
     def ref(self):
@@ -64,7 +64,7 @@ class Board:
                 while self.dictTrou[slct][0] > 0:
                     trot += 1
                     if trot == 13:
-                        trot = 1     
+                        trot = 1
                     self.dictTrou[trot][0] += 1
                     self.dictTrou[slct][0] -= 1
                 print("whoup! \o/\n")
@@ -72,8 +72,7 @@ class Board:
                 (trot>=7 and trot<=12):
                     self.scorePlayer += self.dictTrou[trot][0]
                     self.dictTrou[trot][0] = 0
-                         
+
             else:
                 print("Ce trou est vide!!! (connard)")
 
-        
