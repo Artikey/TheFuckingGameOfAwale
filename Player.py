f4344fe35cbaa38c4dc431641a3721e622061fb0
Artikey <remycharles22@gmail.com>
2018-09-30 17:10:31 +0200

    Implemented the basis of a Roguelike board

--- Player.py
+++ Player.py
@@ -1,6 +1,13 @@
 import pygame
 
 class Player:
-    def __init__(self, x, y):
-        self.X = x
-        self.Y = y
+    def __init__(self, x, y, width, height, vel, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = vel
+        self.color = color
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height))
---
+++ RPGBoard.py
@@ -0,0 +1,32 @@
+import pygame
+from Player import Player
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("RPGStyledBoard")
+
+p = Player(50, 50, 40, 60, 5, (255, 0, 0))
+running = True
+while running:
+    win.fill((0, 0, 0))
+    pygame.time.delay(10)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        p.x -= p.vel
+
+    if keys[pygame.K_d]:
+        p.x += p.vel
+    if keys[pygame.K_w]:
+        p.y -= p.vel
+    if keys[pygame.K_s]:
+        p.y += p.vel
+
+    p.draw(win)
+    pygame.display.update()
+
+pygame.quit()
